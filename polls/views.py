13322c8de7b4bcdea20065da129dde5e944983dd
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-22 18:54:07 +0900

    add urls.py modified views.py desplay one_line_text

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.http import HttpResponse
 
+def index(response):
+    return HttpResponse("Hello, world. You're at the index.")
 # Create your views here.
