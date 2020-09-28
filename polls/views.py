b0df3b3b2f522971880e1d60aca74e8700641d58
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-09-28 10:28:19 +0900

    add_finction Emit a 404 error

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,41 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+#HttpResponseを省略せずに記述する方法
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
 # Create your views here.
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+#上のコードは以下のように省略できる
+#※http404を放出するコードはよく使われるため
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     responce = "You're looking at the results of question %s."
