bbc6549a7aadd3f3aedef0565adf98ab37798196
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-03 18:20:06 +0900

    finish tutorial-4 add generic_view

--- polls/views.py
+++ polls/views.py
@@ -4,16 +4,28 @@ from django.template import loader
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 
+#汎用ビューを使用する
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+
+#汎用ビューを使用しないコード
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return render(request, 'polls/index.html', context)
 
 #HttpResponseを省略せずに記述する方法
 # def index(request):
@@ -25,12 +37,18 @@ def index(request):
 #     return HttpResponse(template.render(context, request))
 # Create your views here.
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+#汎用ビューを使用したコード
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+#汎用ビューを使用しないコード
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
 
 #上のコードは以下のように省略できる
 #※http404を放出するコードはよく使われるため
@@ -38,9 +56,15 @@ def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    responce = "You're looking at the results of question %s."
-    return HttpResponse(responce % question_id)
+#汎用ビューを使うコード
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+#汎用ビューを使用しないコード
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
