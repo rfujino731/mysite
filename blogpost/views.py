6c994d3576ab7f807413fc8b3f117b9fdfaee7e0
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-11 19:31:06 +0900

    add blogpost/detail

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import BlogModel
 
 class BlogList(ListView):
     template_name = 'blogpost/list.html'
+    model = BlogModel
+
+class BlogDetail(DetailView):
+    template_name = 'blogpost/detail.html'
     model = BlogModel
 
 # Create your views here.
