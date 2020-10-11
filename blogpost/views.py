8445a1510118debffc2482b3ea5ef336de849f00
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-11 16:49:18 +0900

    modified blogpost/models,urls,views changed models,urls.viewa for ListView finish migrate 002_blogmodel.py

--- blogpost/views.py
+++ blogpost/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import BlogModel
+
+class BlogList(ListView):
+    template_name = 'list.html'
+    model = BlogModel
 
 # Create your views here.
