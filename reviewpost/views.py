6ae8be39c0201ee3133f6ea55b6c87e027e7ae61
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-18 19:15:15 +0900

    madd reviewpost/models.py excuted migrate of reviewpost/models.py

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def signupview(request):
+    return render(request, 'signup.html', {'somedate':100})
