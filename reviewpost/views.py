064d5cfe9c98ad9e836ff63701ac617e9c6da197
rfujino731 <wbhp.litw.twba.lib@gmail.com>
2020-10-18 23:42:50 +0900

    modified reviewpost/signup.html

--- reviewpost/views.py
+++ reviewpost/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def signupview(request):
+    print(request.POST.get('username_date'))
     return render(request, 'reviewpost/signup.html', {'somedate':100})
